14eb45c352e408efb90bc5e7fe67359b5e4b5513
Wang Khang Waei <devtesting404@gmail.com>
2023-11-23 17:44:48 +0800

    update: shift Colors.ts from src/constants to src/styles

---
+++ src/styles/Colors.ts
@@ -0,0 +1,7 @@
+export const FT_PRIMARY: string = '#00BABC';
+
+export const FT_SECONDARY: string = '#04809f';
+
+export const FT_TEXT: string = '#fff';
+
+export const FT_HEADER_TEXT: string = '#173d7a';
